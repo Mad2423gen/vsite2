3e650423b225074fa255a35fe3214acd57f8eb52
gen <soramimiplatz@gmail.com>
2022-12-10 01:08:32 +0900

    2022-12-08 お問い合わせフォーム完成

--- lesson_blog/settings.py
+++ lesson_blog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jhm#9%*4amt!um98hs64paw53a5g=t2=7j$a^yv7(8_st010-1'
-
+# SECRET_KEY = 'django-insecure-jhm#9%*4amt!um98hs64paw53a5g=t2=7j$a^yv7(8_st010-1'
+SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com', 'vickers.pythonanywhere.com ']
 
 # メールサーバー用
 EMAIL_HOST = 'smtp.gmail.com'
@@ -138,3 +140,10 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+try:
+    from .local_settings import *
+except:
+    pass
